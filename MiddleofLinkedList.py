74c3869d8de30f2b67d5b03be11bd508a5c40fda
sejalnamdev <namdevsejal31@gmail.com>
2026-06-29 22:26:19 +0530

    MiddleoftheLinkedList

---
+++ MiddleofLinkedList.py
@@ -0,0 +1,7 @@
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
